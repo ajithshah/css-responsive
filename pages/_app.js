d5568694f0c723a19eab202d8b77f1318c6e763c
ajithshah <ajithshah37@gmail.com>
2022-10-07 11:38:16 +0530

    [Ajith] Set folder

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import React from "react";
+import Head from "next/head";
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Head>
+        <meta name="theme-color" content="#3c1742" />
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
 
 export default MyApp
